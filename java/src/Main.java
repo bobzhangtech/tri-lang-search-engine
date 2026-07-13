40bf114f452d42be3cb632e9939616f6cea9b474
Bob Zhang <270489537+bobzhangtech@users.noreply.github.com>
2026-07-13 12:01:48 -0700

    Implement hashmap for storing and sorting the output from most to least matches

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         List<String> fileNamesList = new ArrayList<>();
+        Map<String, Integer> outputMap = new HashMap<>();
         String folderName = "sample_data";
 
-        String userInput = "the";
-
         DirectoryScanner.getFileNames(fileNamesList, folderName);
+
+        String userInput = "the"; // For testing only, delete later
 
         for (String fileName : fileNamesList) {
             List<String> wordsList = new ArrayList<>();
@@ -23,7 +26,10 @@ public class Main {
                 }
             }
 
-            System.out.println(fileName + " - " + count);
+            outputMap.put(fileName, count);
         }
+
+        outputMap.entrySet().stream().sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+                .forEach(entry -> System.out.println(entry.getKey() + " - " + entry.getValue()));
     }
 }
